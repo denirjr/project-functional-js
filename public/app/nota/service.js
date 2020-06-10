a0e30c16dbcac9b1fe207d5ad4053878a142f780
Denir <denir-junior19@live.com>
2020-06-10 20:54:37 -0300

    -mfeature(master): Point-free style

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,5 +1,5 @@
 import { handleStatus } from "../ultils/promisse-helpers.js";
-import { partialize } from "../ultils/operators.js";
+import { partialize, compose } from "../ultils/operators.js";
 
 const API = "http://localhost:3000/notas";
 
@@ -22,9 +22,8 @@ export const notasService = {
 
   sumItems(code) {
     const filterItems = partialize(filterItemsByCode, code);
+    const sumItems = compose(sumItemsValue, filterItems, getItemsFromNotes)
 
-    return this.listAll().then((notas) =>
-      sumItemsValue(filterItems(getItemsFromNotes(notas)))
-    );
+    return this.listAll().then(sumItems);
   },
 };
