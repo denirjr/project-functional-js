661e84fffb5ba939af544834d444ba4e46b41ec7
Denir <denir-junior19@live.com>
2020-06-20 13:56:10 -0300

    -mfeature(master): created timeout when slow network using promise race

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,13 @@
-import './ultils/array-helpers.js';
+import "./ultils/array-helpers.js";
 
-import { notasService as service } from './nota/service.js';
-import { debounceTime, partialize, pipe, takeUntil } from './ultils/operators.js';
+import { notasService as service } from "./nota/service.js";
+import {
+  debounceTime,
+  partialize,
+  pipe,
+  takeUntil,
+} from "./ultils/operators.js";
+import { timeoutPromise } from "./ultils/promisse-helpers.js";
 
 const operations = pipe(
   partialize(takeUntil, 3),
@@ -9,7 +15,9 @@ const operations = pipe(
 );
 
 const action = operations(() =>
-  service.sumItems("2143").then(console.log).catch(console.log)
+  timeoutPromise(200, service.sumItems("2143"))
+    .then(console.log)
+    .catch(console.log)
 );
 
 document.querySelector("#myButton").onclick = action;
--- public/app/ultils/promisse-helpers.js
+++ public/app/ultils/promisse-helpers.js
@@ -1,7 +1,16 @@
 export const handleStatus = (res) =>
   res.ok ? res.json() : Promise.reject(res.statusText);
 
-  export const log = param => {
-    console.log(param)
-    return param;
-  }
+export const log = (param) => {
+  console.log(param);
+  return param;
+};
+
+export const timeoutPromise = (milliseconds, promise) => {
+  const timeout = new Promise(
+    (resolve, reject) =>
+      setTimeout(() => reject(`Exceed request ${milliseconds} ms`)),
+    milliseconds
+  );
+  return Promise.race([timeout, promise]);
+};
