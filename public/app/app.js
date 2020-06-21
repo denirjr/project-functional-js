cde4b991fb794853c361c4a5a73ef473e6b368ad
Denir <denir-junior19@live.com>
2020-06-21 20:25:13 -0300

    -mfeature(master): implemented retry and delay functionality

--- public/app/app.js
+++ public/app/app.js
@@ -7,7 +7,7 @@ import {
   pipe,
   takeUntil,
 } from "./ultils/operators.js";
-import { timeoutPromise } from "./ultils/promisse-helpers.js";
+import { timeoutPromise, retry } from "./ultils/promisse-helpers.js";
 
 const operations = pipe(
   partialize(takeUntil, 3),
@@ -15,7 +15,7 @@ const operations = pipe(
 );
 
 const action = operations(() =>
-  timeoutPromise(200, service.sumItems("2143"))
+  retry(3, 3000, () => timeoutPromise(200, service.sumItems("2143")))
     .then(console.log)
     .catch(console.log)
 );
